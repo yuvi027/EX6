307451159152fb1f184b0b68e3a1be45972a433c
agam_e <agam.ebel@mail.huji.ac.il>
2023-01-13 14:10:37 +0200

    Added missing file

---
+++ oop/ex6/main/Sjavac.java
@@ -0,0 +1,25 @@
+package oop.ex6.main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Sjavac {
+
+    private static final int IO_ERROR = 2;
+    private static final int FILE_NAME_IND = 2;
+
+
+    public static void main(String[] args){
+        CodeChecker  checker = CodeChecker.getInstance();
+        //int num = checker.checkCode(args[FILE_NAME_IND]);
+        int num = checker.checkCode("C:\\Users\\agame\\IdeaProjects\\EX6\\oop\\ex6\\main\\test1.txt");
+        if (num == IO_ERROR){
+            System.err.println(checker.getErr());
+        }
+        else{
+            System.out.println(num);
+        }
+
+    }
+}
